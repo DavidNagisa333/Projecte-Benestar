aed42c5edc2846ab5274f85cd34c86456fbc5540
ITE-DCACERES\dcaceres <dcaceres>
2017-09-14 12:42:10 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-<<<<<<< HEAD
-import { ServeisComponent } from './benestar/serveis/serveis.component';
-import { SergiComponent } from './benestar/sergi/sergi.component';
-=======
-import { DavidComponent } from './benestar/david/david.component';
-//import { SergiComponent } from './benestar/sergi/sergi.component';
->>>>>>> c94213f59440cc7579e7174ef6c9976c829ca5b4
 import { JaumeMGComponent } from './benestar/jaume-mg/jaume-mg.component';
 import { JaumeCComponent } from './benestar/jaume-c/jaume-c.component';
 import { LoginComponent } from './login/login.component';
@@ -15,11 +8,6 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
     {
     path: '',
-    children: []
-    },
-    {
-    path: 'app-serveis',
-    component: ServeisComponent,
     children: []
     },    
     {
