22839e3373b18fec3344eb13a8270abce50cb095
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-09-14 13:26:09 +0200

    no message

--- src/app/benestar/jaume-mg/jaume-mg.component.ts
+++ src/app/benestar/jaume-mg/jaume-mg.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { JaumeMGService } from './jaume-mg.service';
 
 @Component({
   selector: 'app-jaume-mg',
   templateUrl: './jaume-mg.component.html',
-  styleUrls: ['./jaume-mg.component.css']
+  styleUrls: ['./jaume-mg.component.css'],
+  providers: [ JaumeMGService ]
 })
 export class JaumeMGComponent implements OnInit {
 
-  constructor() { }
+	nom: string;
+	cognoms: string;
+	naixement: Date;
+	correu: string;
+	telefon: string;
+	mobil: string;
+	adreca: string;
+	localitat: string;
+	comarca: string;
 
-  ngOnInit() {
-  }
+  	constructor(private personaService: JaumeMGService) { }
 
+  	ngOnInit() { }
+
+  	addPersona() {
+  		
+  	}
 }
