a24de3cb55f14212ee59f232a7066c282cd1d5e0
ITE-DCACERES\dcaceres <dcaceres>
2017-09-14 12:46:42 +0200

    no message

--- src/app/benestar/home.module.ts
+++ src/app/benestar/home.module.ts
@@ -22,30 +22,11 @@ import { JaumeCComponent } from './jaume-c/jaume-c.component';
     AppRoutingModule
   ],
   declarations: [
-<<<<<<< HEAD:src/app/benestar/benestar.module.ts
-      BenestarComponent,
       ServeisComponent,
-=======
       HomeComponent,
-      DavidComponent,
->>>>>>> 9d8405588978537ed9c6718921aef4b2086d0b21:src/app/benestar/home.module.ts
       JaumeCComponent,
       OrganitzacionsComponent,
       JaumeMGComponent]
 })
-<<<<<<< HEAD:src/app/benestar/benestar.module.ts
-export class BenestarModule { }
-=======
+
 export class HomeModule { }
-
-
-/*import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from '../app-routing.module';
-import {FormsModule} from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-
-
-import { DavidComponent } from './david/david.component';*/
->>>>>>> 9d8405588978537ed9c6718921aef4b2086d0b21:src/app/benestar/home.module.ts
