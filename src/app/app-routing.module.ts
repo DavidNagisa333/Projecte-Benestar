cf7e8b7c11cfad232aa36cb0eb2ad54e5b99e2a0
ITE-DCACERES\dcaceres <dcaceres>
2017-09-14 09:03:57 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DavidComponent } from './benestar/david/david.component';
+import { SergiComponent } from './benestar/sergi/sergi.component';
+import { JaumeMGComponent } from './benestar/jaume-mg/jaume-mg.component';
+import { JaumeCComponent } from './benestar/jaume-c/jaume-c.component';
+import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [
-  {
+    {
     path: '',
     children: []
-  }
+    },
+    {
+    path: 'app-david',
+    component: DavidComponent,
+    children: []
+    },
+    {
+    path: 'app-sergi',
+    component: SergiComponent,
+    children: []
+    },
+    {
+    path: 'app-jaume-mg',
+    component: JaumeMGComponent,
+    children: []
+    },
+    {
+    path: 'app-jaume-c',
+    component: JaumeCComponent,
+    children: []
+    },
+    {
+    path: 'login',
+    component: LoginComponent,
+    children: []
+    },
 ];
 
 @NgModule({
--- src/app/benestar/benestar.module.ts
+++ src/app/benestar/benestar.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {AppRoutingModule} from '../app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
 import { BenestarRoutingModule } from './benestar-routing.module';
 import { BenestarComponent } from './benestar.component';
+
 import { DavidComponent } from './david/david.component';
+import { SergiComponent } from './sergi/sergi.component';
+import { JaumeMGComponent } from './jaume-mg/jaume-mg.component';
 import { JaumeCComponent } from './jaume-c/jaume-c.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    BenestarRoutingModule
+    BenestarRoutingModule,
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  declarations: [BenestarComponent, DavidComponent, JaumeCComponent]
+  declarations: [
+      BenestarComponent,
+      DavidComponent,
+      JaumeCComponent,
+      SergiComponent,
+      JaumeMGComponent]
 })
 export class BenestarModule { }
+
+
+/*import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from '../app-routing.module';
+import {FormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+
+import { DavidComponent } from './david/david.component';*/
