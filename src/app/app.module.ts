ec228550183d019fb567e14dc1903a1485c5055e
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-09-20 11:41:01 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 import { HomeModule } from './benestar/home.module';
 
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { LoginService } from './login/login.service';
 
 @NgModule({
+  imports: [
+    HomeModule,
+  ],
   declarations: [
     AppComponent,
     LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeModule,
-    HttpModule,
-    FormsModule
-  ],
-  providers: [LoginService],
-  bootstrap: [AppComponent]
+  ]
 })
 export class AppModule { }
--- src/app/benestar/home.module.ts
+++ src/app/benestar/home.module.ts
@@ -8,17 +8,12 @@ import {ServeisModule} from './serveis/serveis.module';
 import {PersonesModule} from './persones/persones.module';
 import {OficinesModule} from './Oficines/oficines.module';
 
-
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
-<<<<<<< HEAD
     HomeRoutingModule,
-          
-=======
-    HomeRoutingModule,    
->>>>>>> 5ac295b8ddcb7657e258789e0f76f53fe5ffbde8
+
     OrganitzacionsModule,
     ServeisModule,
     PersonesModule,
