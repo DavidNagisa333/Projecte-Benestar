9a9cfd23e21f670d685a8ebeb3b34e4cbbb1dd17
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-09-20 11:23:04 +0200

    no message

--- src/app/benestar/home.module.ts
+++ src/app/benestar/home.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 
-
 import { HomeRoutingModule } from './home-routing.module';
 
 import {OrganitzacionsModule} from './organitzacions/organitzacions.module';
 import {ServeisModule} from './serveis/serveis.module';
 import {PersonesModule} from './persones/persones.module';
-import {OficinesModule} from './oficines/oficines.module';
+import {OficinesModule} from './Oficines/oficines.module';
 
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [
     HomeRoutingModule,
-      
+          
     OrganitzacionsModule,
     ServeisModule,
     PersonesModule,
