8c855bf628446bd308faa81991aa0af291e8093f
ITE-DCACERES\dcaceres <dcaceres>
2017-09-20 10:21:24 +0200

    no message

--- src/app/benestar/home.module.ts
+++ src/app/benestar/home.module.ts
@@ -6,17 +6,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
-<<<<<<< HEAD
 
-//import { OrganitzacionsComponent } from './organitzacions/organitzacions.component';
-import { JaumeMGComponent } from './jaume-mg/jaume-mg.component';
-import { JaumeCComponent } from './jaume-c/jaume-c.component';
-=======
 import { ServeisComponent } from './serveis/serveis.component';
 import { OrganitzacionsComponent } from './organitzacions/organitzacions.component';
 import { PersonesComponent, Ordre } from './persones/persones.component';
 import { OficinesComponent } from './Oficines/oficines.component';
->>>>>>> 46a63b550bef7e9e6228098a75b53317f058cb9b
+
 import {OrganitzacionsModule} from './organitzacions/organitzacions.module';
 import { ServeisModule } from './serveis/serveis.module';
 //import {ProvaComponent} from './organitzacions/prova/prova.component';
@@ -36,9 +31,5 @@ import { ServeisModule } from './serveis/serveis.module';
       OficinesComponent]
 
 })
-<<<<<<< HEAD
-=======
-
->>>>>>> 46a63b550bef7e9e6228098a75b53317f058cb9b
 
 export class HomeModule { }
