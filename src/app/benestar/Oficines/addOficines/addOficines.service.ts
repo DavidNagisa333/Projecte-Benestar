a4902e6f9075131c7faa4827d49822cd074b653b
ITE-JCALLE\jcalle <jcalle@teknos.cat>
2017-09-28 11:13:12 +0200

    no message

--- src/app/benestar/Oficines/addOficines/addOficines.service.ts
+++ src/app/benestar/Oficines/addOficines/addOficines.service.ts
@@ -1,10 +1,6 @@
 import {Injectable}  from '@angular/core';
 import {Http,Headers,Response, RequestOptions}  from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-<<<<<<< HEAD
-
-=======
->>>>>>> 24e20f9daad038557b63940aed2e9839a96bab27
 
 @Injectable()
 
@@ -25,11 +21,9 @@ addOficina(oficina){   var headers = new Headers();
       	return this.http
       			   //.post(url, JSON.stringify(oficina), options)
       			   .post(this.addUrl, body);
-<<<<<<< HEAD
-=======
+
 
 
                    //.map(res => res.json());
->>>>>>> 24e20f9daad038557b63940aed2e9839a96bab27
 	}
 }
